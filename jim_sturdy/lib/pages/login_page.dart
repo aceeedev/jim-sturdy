a8be56f44d130eb92bbb3e8efd168b380b31c85c
aceeedev <cyberandrew03@gmail.com>
2024-03-28 00:18:41 -0700

    Added firebase login auth

---
+++ jim_sturdy/lib/pages/login_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  ValueNotifier userCredential = ValueNotifier('');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Login')),
+        body: ValueListenableBuilder(
+            valueListenable: userCredential,
+            builder: (context, value, child) {
+              return (userCredential.value == '' ||
+                      userCredential.value == null)
+                  ? Center(
+                      child: Card(
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: IconButton(
+                          iconSize: 40,
+                          icon: const Icon(Icons.login),
+                          onPressed: () async {
+                            userCredential.value = await signInWithGoogle();
+                            if (userCredential.value != null)
+                              print(userCredential.value.user!.email);
+                          },
+                        ),
+                      ),
+                    )
+                  : Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            clipBehavior: Clip.antiAlias,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                    width: 1.5, color: Colors.black54)),
+                            child: Image.network(
+                                userCredential.value.user!.photoURL.toString()),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(userCredential.value.user!.displayName
+                              .toString()),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(userCredential.value.user!.email.toString()),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          ElevatedButton(
+                              onPressed: () async {
+                                bool result = await signOutFromGoogle();
+                                if (result) userCredential.value = '';
+                              },
+                              child: const Text('Logout'))
+                        ],
+                      ),
+                    );
+            }));
+  }
+
+  Future<dynamic> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } on Exception catch (e) {
+      print('exception: $e');
+    }
+  }
+
+  Future<bool> signOutFromGoogle() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      return true;
+    } on Exception catch (_) {
+      return false;
+    }
+  }
+}
